df474645eaad0d2bb7cee6f08a143ac49be8bd36
Kosuke Endo <info@gofw.jp>
2026-03-12 00:06:28 +0900

    Update SHA256 with pre-signed app, remove postflight codesign

--- Casks/ghostride.rb
+++ Casks/ghostride.rb
@@ -1,6 +1,6 @@
 cask "ghostride" do
   version "1.0.0"
-  sha256 "586c1e70ee6b63045db3097bffbcbbac11e29503c0e28d6c23a97aa117b1cd1d"
+  sha256 "2e98890fda672f70d1b120fc1d7f1cdf3560e671124bda002355f555d3daf4ce"
 
   url "https://github.com/goforward227-ctrl/ghostride/releases/download/v#{version}/Ghostride-#{version}-arm64.zip"
   name "Ghostride"
@@ -15,9 +15,6 @@ cask "ghostride" do
   postflight do
     system_command "/usr/bin/xattr",
                    args: ["-cr", "#{appdir}/Ghostride.app"],
-                   sudo: false
-    system_command "/usr/bin/codesign",
-                   args: ["--deep", "--force", "--sign", "-", "#{appdir}/Ghostride.app"],
                    sudo: false
   end
 
